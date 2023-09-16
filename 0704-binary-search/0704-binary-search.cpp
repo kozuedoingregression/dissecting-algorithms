319b8b2ad73d3cdb20fe09209496e99bb5e3a5b3
Sashank Thapa <91571529+kozuedoingregression@users.noreply.github.com>
2023-09-16 14:15:04 +0530

    Time: 19 ms (97.19%), Space: 27.8 MB (11.98%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int middle = (left + right) / 2;
+        int i=0;
+        int j=nums.size() -1;
 
-        while (left <= right) {
-            if (nums[middle] == target)
-                return middle;
-            if (target > nums[middle])
-                left = middle + 1;
-            else if (target < nums[middle])
-                right = middle - 1;
-            
-            middle = (left + right) / 2;
+        while(i<=j)
+        {
+            int mid = (i+j)/2;
+            if(target > nums[mid]) i = mid +1;
+            else if( target < nums[mid]) j = mid - 1;
+            else return mid;
         }
-
         return -1;
     }
 };
